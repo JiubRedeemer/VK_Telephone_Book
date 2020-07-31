d41a89e53d76303d2ece2ba4cd0033ccf9e36080
Vlad <vladok200202@gmail.com>
2020-07-31 16:01:49 +0400

    fixed

--- VK_TelephoneBook v0.2/keys.py
+++
@@ -1,4 +0,0 @@
-vk_token = "6b5a5cbb0636e93ea4895c1aecd0b26f049768694e1bdf4a7af92e7b62a8b7549dacca0f2be5f729b71e1"
-HMAC = b'2Wq7)qkX~cp7)H|n_tc&o+:G_USN3/-uIi~>M+c ;Oq]E{t9)RC_5|lhAA_Qq%_4'
-cipher_k = 'MjsDska724DI1tKFwVx/vFVnF071b7jYUbNJYqMGpSY='
-gc_token = "NcHta397f6336cbd50d7314a8db001a86948daafee33d961eca85765d39d"
